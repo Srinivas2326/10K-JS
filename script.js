ac2a159de1a04a3afeaa09f3657bc842a2b3e27a
srinivas peddada <srinivas23peddada@gmail.com>
2025-08-01 11:14:40 +0530

    javascript codes are added

---
+++ script.js
@@ -0,0 +1,36 @@
+//1.  Positive, negative, and zero values
+// write a program to check whether a number is positive, negative or zero.
+
+// let a = prompt("Enter a number: ")
+
+// if (a>0){
+//     document.write(a + " is a positive number")
+// }
+// ________________________________________________________________________________________________-
+
+//2.  largest of two numbers
+// var c = prompt("Enter first number")
+// var d = prompt("Enter second number")
+
+// if (c>d){
+//     document.write(c)
+// }
+// else{
+//     document.write(d)
+// }
+// ________________________________________________________________________________________________-
+
+// 3. largest of three numbers
+// const e = prompt("Enter first number")
+// const f = prompt("Enter second number")
+// const g = prompt("Enter third number")
+
+// if (e>f){
+//     document.write(e)
+// }
+// else if (f>g){
+//     document.write(f)
+// }
+// else{
+//     document.write(g)
+// }

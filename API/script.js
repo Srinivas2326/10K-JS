c780ee057e7962fef10f260ee51d58efbf88ec8d
srinivas peddada <srinivas23peddada@gmail.com>
2025-09-01 11:50:38 +0530

    buttons are updated

--- API/script.js
+++ API/script.js
@@ -1,49 +1,61 @@
-
 let user_container = document.getElementById("userdata")
 let addBtn = document.getElementById("add")
-let deleteBtn = document.getElementById("dlte")
-let updateBtn = document.getElementById("update")
 
-
-fetch("https://gorest.co.in/public/v2/users",{
+// Fetch and display users
+fetch("https://gorest.co.in/public/v2/users", {
   method: "GET",
-    headers:{
-      Authorization: "Bearer 35f70ed1edc2922a5711e9ec54249cc62fee316524ff00c7dc87130f94838872"
-    },
+  headers: {
+    Authorization: "Bearer 35f70ed1edc2922a5711e9ec54249cc62fee316524ff00c7dc87130f94838872"
+  },
 })
-.then(function(response){
-  return response.json()
-})
-.then(function(jsondata){
+.then(response => response.json())
+.then(jsondata => {
   console.log(jsondata)
-  for(let i=0; i<jsondata.length; i++){
+  user_container.innerHTML = ""; // clear before loading
+  jsondata.forEach(user => {
     let card = document.createElement("div")
-    card.classList="card"
-    card.innerHTML=`
-        <p>${jsondata[i].id}</p>
-        <p>${jsondata[i].name}</p>
-        <p>${jsondata[i].email}</p>
-        <p>${jsondata[i].gender}</p>
-        
+    card.classList = "card"
+    card.innerHTML = `
+      <p>${user.id}</p>
+      <p>${user.name}</p>
+      <p>${user.email}</p>
+      <p>${user.gender}</p>
+      <div class="card-buttons">
+        <button class="delete-btn" data-id="${user.id}">Delete</button>
+        <button class="update-btn" data-id="${user.id}">Update</button>
+      </div>
     `
     user_container.appendChild(card)
-  }
-})
+  });
+
+  // Attach delete button events
+  document.querySelectorAll(".delete-btn").forEach(btn => {
+    btn.addEventListener("click", function() {
+      let userId = this.getAttribute("data-id");
+      deleteUser(userId);
+    });
+  });
+
+  // Attach update button events
+  document.querySelectorAll(".update-btn").forEach(btn => {
+    btn.addEventListener("click", function() {
+      let userId = this.getAttribute("data-id");
+      updateUser(userId);
+    });
+  });
+});
 
 
-// To add new data
-
+// Add new user
 function addUser(){
-let user = {
-      name : "srinuivas",
-      email: "srinuivas@gmail.com",
-      gender: "male",
-      status: "active"
-    }
+  let user = {
+    name : "sanju",
+    email: "sanju@gmail.com",
+    gender: "male",
+    status: "active"
+  }
 
-
-fetch("https://gorest.co.in/public/v2/users",
-  {
+  fetch("https://gorest.co.in/public/v2/users", {
     method: "POST",
     headers:{
       "Content-type":"application/json",
@@ -52,65 +64,54 @@ fetch("https://gorest.co.in/public/v2/users",
     },
     body: JSON.stringify(user)
   })
-
-  .then(function(res){
-return res.json()
-  })
-
-.then(function(jsondata){
-  console.log(jsondata)
-})
-}
-// ___________________________________________________________________________________
-
-// how to delete a user
-
-function deleteUser(){
-fetch("https://gorest.co.in/public/v2/users/8099696",{
-  method:"DELETE",
-  headers:{
-    Accept: "application/json",
-    Authorization: "Bearer 35f70ed1edc2922a5711e9ec54249cc62fee316524ff00c7dc87130f94838872"
-  }
-})
-.then(function(res){
-  console.log(res)
-})
+  .then(res => res.json())
+  .then(jsondata => {
+    console.log(jsondata);
+    location.reload(); // reload to see new user
+  });
 }
 
-// ______________________________________________________________________________________________________________________
 
-// UPDATE DATA
-
-function updateUser(){
-let user = {
-      gender:"male",
-      name:"srinivasPeddada",
-      email:"srinivas1234peddada@gmail.com"
-      
+// Delete user
+function deleteUser(userId){
+  fetch(`https://gorest.co.in/public/v2/users/${userId}`, {
+    method:"DELETE",
+    headers:{
+      Accept: "application/json",
+      Authorization: "Bearer 35f70ed1edc2922a5711e9ec54249cc62fee316524ff00c7dc87130f94838872"
     }
-
-
-fetch("https://gorest.co.in/public/v2/users/8099810",{
-  method:"PUT",
-  body: JSON.stringify(user),
-  headers:{
-    "Content-type": "application/json",
-    Accept: "application/json",
-    Authorization: "Bearer 35f70ed1edc2922a5711e9ec54249cc62fee316524ff00c7dc87130f94838872",
-  }
-})
-.then(function(res){
-  return res.json()
-})
-.then(function(jsondata){
-  console.log(jsondata)
-})
+  })
+  .then(res => {
+    console.log(res);
+    location.reload(); // reload after delete
+  });
 }
 
 
+// Update user
+function updateUser(userId){
+  let user = {
+    gender:"male",
+    name:"P.Srinivas",
+    email:"psrinivaspeddada@gmail.com"
+  }
+
+  fetch(`https://gorest.co.in/public/v2/users/${userId}`, {
+    method:"PUT",
+    body: JSON.stringify(user),
+    headers:{
+      "Content-type": "application/json",
+      Accept: "application/json",
+      Authorization: "Bearer 35f70ed1edc2922a5711e9ec54249cc62fee316524ff00c7dc87130f94838872",
+    }
+  })
+  .then(res => res.json())
+  .then(jsondata => {
+    console.log(jsondata);
+    location.reload(); // reload to see update
+  });
+}
 
 
-addBtn.addEventListener("click", addUser)
-deleteBtn.addEventListener("click", deleteUser)
-updateBtn.addEventListener("click", updateUser)
+// Add button only for new users
+addBtn.addEventListener("click", addUser);

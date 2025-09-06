6894e5e88d44bba708bbe99f794d491d2523d445
srinivas peddada <srinivas23peddada@gmail.com>
2025-09-06 22:19:55 +0530

    styling is updated

--- quiz/script.js
+++ quiz/script.js
@@ -8,7 +8,6 @@ const questions = [
             { text: "JavaScript", correct: true }
         ]
     },
-
     {
         question: "What does CSS stand for?",
         answers: [
@@ -18,7 +17,6 @@ const questions = [
             { text: "Cars SUVs Sailboats", correct: false }
         ]
     },
-
     {
         question: "What year was JavaScript launched?",
         answers: [
@@ -28,7 +26,6 @@ const questions = [
             { text: "none of the above", correct: false }
         ]
     },
-
     {
         question: "What does DOM stand for?",
         answers: [
@@ -38,7 +35,6 @@ const questions = [
             { text: "Desktop Oriented Mode", correct: false }
         ]
     },
-
     {
         question: "Which company developed JavaScript?",
         answers: [
@@ -50,10 +46,9 @@ const questions = [
     }
 ];
 
-
 const questionElement = document.getElementById("question");
-const answerButtons = document.getElementById("answer-buttons")
-const nextButton = document.getElementById("next-btn")
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
 
 let currentQuestionIndex = 0;
 let score = 0;
@@ -61,86 +56,79 @@ let score = 0;
 function startQuiz() {
     currentQuestionIndex = 0;
     score = 0;
-    nextButton.innerHTML = "Next"
-    showQuestion()
+    nextButton.innerHTML = "Next";
+    showQuestion();
 }
 
-function showQuestion(){
-    resetState()
+function showQuestion() {
+    resetState();
     let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
-    questionElement.innerHTML = questionNo + ". " + currentQuestion.question
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
 
-    currentQuestion.answers.forEach(answer=>{
+    currentQuestion.answers.forEach(answer => {
         const button = document.createElement("button");
         button.innerHTML = answer.text;
         button.classList.add("btn");
         answerButtons.appendChild(button);
-        if(answer.correct){
+        if (answer.correct) {
             button.dataset.correct = answer.correct;
         }
-        button.addEventListener("click", selectAnswer)
-    })
+        button.addEventListener("click", selectAnswer);
+    });
 }
 
-
-function resetState(){
+function resetState() {
     nextButton.style.display = "none";
-    while(answerButtons.firstChild){
-        answerButtons.removeChild(answerButtons.firstChild)
+    while (answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild);
     }
 }
 
-
-function selectAnswer(e){
+function selectAnswer(e) {
     const selectBtn = e.target;
     const isCorrect = selectBtn.dataset.correct === "true";
-    if(isCorrect){
+    if (isCorrect) {
         selectBtn.classList.add("correct");
         score++;
+    } else {
+        selectBtn.classList.add("incorrect");
     }
-    else{
-        selectBtn.classList.add("incorrect")
-    }
-    Array.from(answerButtons.children).forEach(button=>{
-        if(button.dataset.correct === "true"){
-            button.classList.add("correct")
+    Array.from(answerButtons.children).forEach(button => {
+        if (button.dataset.correct === "true") {
+            button.classList.add("correct");
         }
         button.disabled = true;
-    })
-    nextButton.style.display = "block"
+    });
+
+    if (currentQuestionIndex === questions.length - 1) {
+        nextButton.innerHTML = "Submit";
+    }
+    nextButton.style.display = "block";
 }
 
-
-
-
-function showScore(){
+function showScore() {
     resetState();
     questionElement.innerHTML = `You Scored ${score} out of ${questions.length}!`;
-    nextButton.innerHTML = "Play Again"
-    nextButton.style.display = "block"
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
 }
 
-
-function handleNextButton(){
+function handleNextButton() {
     currentQuestionIndex++;
-    if(currentQuestionIndex < questions.length){
-        showQuestion()
-    }
-    else{
-        showScore()
+    if (currentQuestionIndex < questions.length) {
+        showQuestion();
+    } else {
+        showScore();
     }
 }
 
-
-nextButton.addEventListener("click", ()=>{
-    if(currentQuestionIndex < questions.length){
-        handleNextButton()
+nextButton.addEventListener("click", () => {
+    if (currentQuestionIndex < questions.length) {
+        handleNextButton();
+    } else {
+        startQuiz();
     }
-    else{
-        startQuiz()
-    }
-})
-
+});
 
 startQuiz();
